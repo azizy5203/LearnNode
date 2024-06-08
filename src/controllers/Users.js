8bd66ad63bceee7cc924bb3c06bbd243236799c9
Youssef Abdulaziz <azizy5203@gmail.com>
2024-06-08 12:24:42 +0300

    refactor : moved logic to controllers, better router structure

--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -1,45 +1,66 @@
 import User from "../models/UserModel.js";
 
-async function createUser(user) {
-  try {
-    const createdUser = await User.create(user);
-    return createdUser;
-  } catch (error) {
-    console.log("🚀 ~ createUser ~ error:", error);
-    return error;
-  }
-}
-async function getAllUsers() {
+async function getAllUsers(req, res, next) {
   try {
     const allUsers = await User.find({});
-    return allUsers;
+    res.status(200).json(allUsers);
   } catch (error) {
     console.log("🚀 ~ getAllUsers ~ error:", error);
-    return error;
+    const err = new Error(error);
+    err.status = 500;
+    return next(err);
+  }
+}
+async function createUser(req, res, next) {
+  try {
+    const userBody = req.body;
+    const createdUser = await User.create(userBody);
+    res.status(200).json(createdUser);
+  } catch (error) {
+    console.log("🚀 ~ createUser ~ error:", error);
+    const err = new Error(error);
+    err.status = 400;
+    return next(err);
   }
 }
 
-async function getOneUser(id) {
+async function getOneUser(req, res, next) {
   try {
-    const user = await User.findById(id);
-    return user;
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+    res.status(200).json(user);
   } catch (error) {
     console.log("🚀 ~ getOneUser ~ error:", error);
-    return error;
+    const err = new Error(error);
+    err.status = 400;
+    return next(err);
   }
 }
 
-async function updateUser(user) {
+async function updateUser(req, res, next) {
   try {
+    const user = req.body;
     const upadetedUser = await User.findByIdAndUpdate(user._id, user);
-    return upadetedUser;
+    res.status(200).json(upadetedUser);
   } catch (error) {
     console.log("🚀 ~ updateUser ~ error:", error);
-    return error;
+    const err = new Error(error);
+    err.status = 400;
+    return next(err);
   }
 }
 
-async function deleteUser(id) {
+async function deleteUser(req, res, next) {
+  try {
+    const userId = req.params.id;
+    const deletedUser = await User.findByIdAndDelete(userId);
+    res.status(200).json(deletedUser);
+  } catch (error) {
+    console.log("🚀 ~ deleteUser ~ error:", error);
+    const err = new Error(error);
+    err.status = 400;
+    return next(err);
+  }
   try {
     const deletedUser = await User.findByIdAndDelete(id);
     return deletedUser;
--- src/router/users.js
+++ src/router/users.js
@@ -1,73 +1,22 @@
 import express from "express";
-import { faker } from "@faker-js/faker";
 
 import {
-  createUser,
   getAllUsers,
+  createUser,
   getOneUser,
-  deleteUser,
   updateUser,
+  deleteUser,
 } from "../controllers/Users.js";
 
 const router = express.Router();
 
-router.get("/GetAll", async (req, res, next) => {
-  try {
-    const allUsers = await getAllUsers();
-    res.status(200).json(allUsers);
-  } catch (error) {
-    const err = new Error(error);
-    err.status = 500;
-    return next(err);
-  }
-});
+router.get("/GetAll", getAllUsers);
 
-router.get("/GetOne/:id", async (req, res, next) => {
-  try {
-    const userId = req.params.id;
-    const user = await getOneUser(userId);
-    res.status(200).json(user);
-  } catch (error) {
-    const err = new Error(error);
-    err.status = 400;
-    return next(err);
-  }
-});
+router.post("/Add", createUser);
 
-router.post("/Add", async (req, res, next) => {
-  const userBody = req.body;
-  // const isDuplicate =
+router.get("/GetOne/:id", getOneUser);
 
-  try {
-    const user = await createUser(req.body);
-    res.status(200).json(user);
-  } catch (error) {
-    const err = new Error(error);
-    err.status = 400;
-    return next(err);
-  }
-});
+router.put("/update", updateUser);
 
-router.put("/update", async (req, res, next) => {
-  try {
-    const updatedUser = await updateUser(req.body);
-    res.status(200).json(updateUser);
-  } catch (error) {
-    const err = new Error(error);
-    err.status = 400;
-    return next(err);
-  }
-});
-
-router.delete("/delete/:id", async (req, res, next) => {
-  try {
-    const userId = req.params.id;
-    const deletedUser = await deleteUser(userId);
-    res.status(200).json(deletedUser);
-  } catch (error) {
-    const err = new Error(error);
-    err.status = 400;
-    return next(err);
-  }
-});
+router.delete("/delete/:id", deleteUser);
 export default router;
