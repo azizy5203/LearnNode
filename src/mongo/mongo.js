8be55e23bf26909b76d807329f8ad014e26af165
Youssef Abdulaziz <azizy5203@gmail.com>
2024-06-10 09:53:55 +0300

    mongo env

--- src/mongo/mongo.js
+++ src/mongo/mongo.js
@@ -2,8 +2,7 @@
 
 import mongoose from "mongoose";
 
-const mongoConnectionUri =
-  "mongodb+srv://youssuf:Sambasong123654@learnnode.kuuvs.mongodb.net/?retryWrites=true&w=majority&appName=LearnNode";
+const mongoConnectionUri = process.env.Mongo_URI;
 // const mongoConnectionUri = process.env.Mongo_URI;
 export default function connect() {
   return mongoose.connect(mongoConnectionUri);
