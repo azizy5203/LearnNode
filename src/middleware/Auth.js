80fc76b30dbf1ac83cb3e7633c3f9e605f6269b9
Youssef Abdulaziz <azizy5203@gmail.com>
2024-07-27 16:03:40 +0300

    refactor : redability improvment

--- src/middleware/Auth.js
+++ src/middleware/Auth.js
@@ -3,8 +3,8 @@ function isAdmin(req, res, next) {
   const token = req.headers.authorization.replace("Bearer ", "");
   if (!token) return res.status(401).send({ error: "not authorized" });
   const decoded = decode(token);
-  if (!decoded?.IsAdmin)
-    return res.status(401).send({ error: "not authorized" });
+  if (decoded.IsAdmin == false)
+    return res.status(401).send({ error: "not authorized for admin access" });
   next();
 }
 export { isAdmin };
